0a7a4de4e7710fd579a46fa44fdc0e60b7cec60e
Jun <119119517+FEABE@users.noreply.github.com>
2023-01-02 16:52:19 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
-<<<<<<< HEAD
 import { AuthModule } from './auth/auth.module';
-
-=======
- 
->>>>>>> efda6d758b1aa07a93ba72e3a2ff4f7d6c400672
 @Module({
   imports: [BoardsModule, AuthModule],
   controllers: [],
